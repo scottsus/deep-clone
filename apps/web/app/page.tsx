62c3deacf689c8d13c84c12d5873f997d31de5fd
scott <susantoscott@gmail.com>
2024-12-02 11:09:54 -0800

    vad working here

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,32 +1,14 @@
-import { Button } from "@repo/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@repo/ui/card";
+"use client";
 
-export default function HomePage() {
+import dynamic from "next/dynamic";
+
+const Demo = dynamic(() => import("./vad"), { ssr: false });
+
+export default async function HomePage() {
   return (
     <main className="flex size-full flex-1 flex-col items-center justify-center gap-y-6">
-      <h1>Hello there Shadcn 👋</h1>
-
-      <Card className="w-1/4">
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-
-      <Button>Button</Button>
+      <h1 className="text-xl font-semibold">Greetings from Deep Clone 🎙</h1>
+      <Demo />
     </main>
   );
 }
