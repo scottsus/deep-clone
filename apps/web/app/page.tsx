09a62479f24babaafc8227bca021cb0aa0f99b1a
scott <susantoscott@gmail.com>
2024-12-02 11:06:41 -0800

    finally got vad to work 🎙️
    1. Shader source issue was resolved by upgrading next to ^15
    
    2. ort-wasm-simd-threaded.mjs was resolved by adding a 'resolutions'
       field in package.json for selective dependency resolution

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { Button } from "@repo/ui/components/ui/button";
+import dynamic from "next/dynamic";
 import Link from "next/link";
 
-export default async function HomePage() {
+import Demo from "./vad";
+
+export default function HomePage() {
   return (
     <main className="flex size-full flex-1 flex-col items-center justify-center gap-y-6">
       <h1 className="text-xl font-semibold">Greetings from Deep Clone 🎙</h1>
       <Link href="/scottsus">
         <Button>Speak with Scott&apos;s clone</Button>
       </Link>
+      <Demo />
     </main>
   );
 }
