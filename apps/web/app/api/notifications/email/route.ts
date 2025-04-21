676c21ebea2eee083d75bdacb03ffe69a89a698d
scott <susantoscott@gmail.com>
2025-04-20 19:48:26 -0700

    implement mailjet server on web app 📫

--- apps/web/app/api/notifications/email/route.ts
+++ apps/web/app/api/notifications/email/route.ts
@@ -1,23 +1,12 @@
-import { resend } from "~/lib/email/resend";
+import { sendEmail } from "~/lib/email/mailjet";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const { guestName } = await req.json();
-    const data = { success: true };
+    console.log(`Sending notification email to ${guestName}`);
 
-    console.log(`Sent notification email to ${guestName}`);
-
-    // const { data, error } = await resend.emails.send({
-    //   from: "Acme <onboarding@resend.dev>",
-    //   to: "Scott Susanto <scottsus@usc.edu>",
-    //   subject: "Deep Clone: new user 🚀",
-    //   text: `${guestName} just tried out Deep Clone!`,
-    // });
-    // if (error) {
-    //   return NextResponse.json(error, { status: 400 });
-    // }
-
+    const data = await sendEmail({ guestName });
     return NextResponse.json(data, { status: 200 });
   } catch (err) {
     console.error("/api/notifications/email hit, error", err);
---
+++ apps/web/lib/email/mailjet.ts
@@ -0,0 +1,32 @@
+import { Client } from "node-mailjet";
+
+const apiKey = process.env.MAILJET_API_KEY;
+const apiSecret = process.env.MAILJET_API_SECRET;
+if (!apiKey || !apiSecret) {
+  throw new Error("missing MAILJET_API_KEY or MAILJET_API_SECRET");
+}
+
+const mailjet = new Client({ apiKey, apiSecret });
+
+export async function sendEmail({ guestName }: { guestName: string }) {
+  const result = await mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: "susantoscott@gmail.com",
+          Name: "Scott Susanto",
+        },
+        To: [
+          {
+            Email: "scottsus@usc.edu",
+            Name: "Scott Susanto",
+          },
+        ],
+        Subject: `🎙️ ${guestName} just tried out Deep Clone!`,
+        HTMLPart: `${guestName} just tried out Deep Clone. See <a>https://dashboard.daily.co/recordings</a>`,
+      },
+    ],
+  });
+
+  return result.body;
+}
